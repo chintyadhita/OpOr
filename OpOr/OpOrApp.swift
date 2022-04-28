a2256dc7644e4426496231a721e6e7ccd5b1b4c9
chintyadhita <chintyadhitaaa@gmail.com>
2022-04-28 09:44:14 +0700

    Initial Commit

---
+++ OpOr/OpOrApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpOrApp.swift
+//  OpOr
+//
+//  Created by Cyntya Rahma Dita on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct OpOrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
